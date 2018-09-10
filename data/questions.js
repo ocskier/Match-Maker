aa87074e6b0b8faf8d6c29d6cc5eb6c4921d5e3d
Pack Fan 1031 <ocskier@gmail.com>
2018-09-10 16:33:14 -0400

    latest revision

---
+++ data/questions.js
@@ -0,0 +1,3 @@
+var questions = [{question: "Would you like to be famous?"},{question:"Question 2"},{question:"Question 3"},{question:"Question 4"},{question:"Question 5"},{question:"Question 6"},{question:"Question 7"},{question:"Question 8"},{question:"Question 9"},{question:"Question 10"}];
+
+module.exports = questions;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,40 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+
+var path = require("path");
+
+var quests = require("../data/questions");
+
+var exphbs = require("express-handlebars");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
+
+  app.get("/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/friends.html"));
+  });
+
+  app.get("/questions", function(req, res) {
+
+    res.render("questions",{quests});
+
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.render("index",quests);
+  });
+};
