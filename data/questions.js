0a0d30497a76c2c045b24ce2a5f347c03625c91b
Pack Fan 1031 <ocskier@gmail.com>
2018-09-20 18:39:33 -0400

    latest revision

--- data/questions.js
+++ data/questions.js
@@ -1,5 +1,5 @@
 var questions = [{question: "Would you like to be famous?"},{question:"Do you consider yourself intelligent?"},{question:"The most imortant thing in my life is my career!"},
-    {question:"The most important thing in my life is having fun?"},{question:"How many children do you see yourself having?"},{question:"Family time is everyhting to me?"},
+    {question:"The most important thing in my life is having fun?"},{question:"How many children do you see yourself having?"},{question:"Family time is everything to me?"},
     {question:"Intimacy is of utmost importance!"},{question:"How many drinks a week do you consume?"},{question:"Do you enjoy athletic activity?"},
     {question:"Are you comfortable with PDA?"}];
 
