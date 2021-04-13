4e284cce5c201e45ff250e3ea33bd4706364e14e
Jon Jackson <ocskier@gmail.com>
2021-04-13 17:04:54 -0400

    chore: update package

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,26 +4,25 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsData = require("../data/friendsData");
-var matchListData = require("../data/matchListData");
-
+var friendsData = require('../data/friendsData');
+var matchListData = require('../data/matchListData');
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get('/api/friends', function (req, res) {
     res.json(friendsData);
   });
 
-  app.get("/api/match", function(req, res) {
+  app.get('/api/match', function (req, res) {
     res.json(matchListData);
   });
 
@@ -35,30 +34,29 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+  app.post('/api/friends', function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
 
-      friendsData.push(req.body);
-      return res.json(friendsData);
-      
+    friendsData.push(req.body);
+    return res.json(friendsData);
   });
 
-  app.post("/api/match", function(req, res) {
+  app.post('/api/match', function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
 
-      matchListData=[req.body];
-      return res.json(matchListData);
+    matchListData = [req.body];
+    return res.json(matchListData);
   });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function(req, res) {
+  app.post('/api/clear', function (req, res) {
     // Empty out the arrays of data
     friendsData.length = [];
     matchListData.length = [];
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,36 +3,38 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 
-var path = require("path");
+var path = require('path');
 
-var quests = require("../data/questions");
+var quests = require('../data/questions');
 // var match = require("../data/matchListData");
-
-var exphbs = require("express-handlebars");
+const requestIp = require('request-ip');
+var exphbs = require('express-handlebars');
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-  app.set("view engine", "handlebars");
+  app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+  app.set('view engine', 'handlebars');
 
-  app.get("/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/friends.html"));
+  app.get('/friends', function (req, res) {
+    const clientIp = requestIp.getClientIp(req);
+    console.log(clientIp);
+    res.sendFile(path.join(__dirname, '../public/friends.html'));
   });
 
-  app.get("/questions", function(req, res) {
-    res.render("questions", { quests });
+  app.get('/questions', function (req, res) {
+    res.render('questions', { quests });
   });
 
   // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.render("index", quests);
+  app.get('*', function (req, res) {
+    res.render('index', quests);
   });
 };
